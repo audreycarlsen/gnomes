f565481a0fd6b2678074a3047276a015b17f1a29
Audrey Carlsen <audreycarlsen@gmail.com>
2014-02-10 10:50:17 -0800

    Gnome's first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gnomes::Application.config.secret_key_base = '9b31d86d6aa5bec0e1ead919c2a81dc73d7adf303b01c5c37533a68d67cfca6d355bbd737b2b4da80e168717d6e9d383186868a7055c85734e83ef5951519469'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gnomes::Application.config.session_store :cookie_store, key: '_gnomes_session'
