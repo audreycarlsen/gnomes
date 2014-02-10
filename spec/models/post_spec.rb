c3850c8c7ec25aa7053a42a84d8f76a8d1f7270c
Galina <yermichevagalina@gmail.com>
2014-02-10 15:56:37 -0800

    validates posts title and content

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "post" do
+
+    let(:post){ create(:post) }
+
+    describe "validates" do
+      it "is valid" do
+        expect(post).to be_valid
+      end
+
+      it "must have a title" do
+        post.update(title: nil)
+        expect(post).to be_invalid
+      end
+
+      it "must have content" do
+        post.update(content: nil)
+        expect(post).to be_invalid
+      end
+    end
+  end
 end
