652084c679cea9fce41c614b528b10ed10c65531
Galina <yermichevagalina@gmail.com>
2014-02-11 11:14:04 -0800

    refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,14 @@ class PostsController < ApplicationController
 
 
   def index
+     @posts = Post.all.order('created_at DESC')
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
-    @posts = Post.all.order('created_at DESC')
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Gnomes::Application.routes.draw do
-  resources :posts
 
   root 'posts#index'
 
-  resources :users
-
   get '/auth/twitter',                                   as: 'sign_in'
-  get '/auth/:provider/callback', to: 'session#create'
   get '/sign_out',                to: 'session#destroy', as:'sign_out'
+  get '/auth/:provider/callback', to: 'session#create'
+
+  resources :users
+  resources :posts
 end
