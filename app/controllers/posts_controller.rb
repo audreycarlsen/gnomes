7a98dfe3e141dc2d159d28daef76852cf3cd3906
Galina <yermichevagalina@gmail.com>
2014-02-10 16:10:34 -0800

    added new, show, create actions to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path, notice: 'Post has been successfully created!'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,48 @@
 require 'spec_helper'
 
 describe PostsController do
+  describe "GET 'new'" do
+    it "is successful" do
+      get :new
+      expect(response).to be_successful
+    end
+  end
 
+  describe "POST 'create'" do
+    context "with valid attributes" do
+      let(:valid_attributes) { {title: "Breaking News", content: "Some things happened"} }
+      it "is a redirect" do
+        post :create, post: valid_attributes
+        expect(response.status).to eq 302 # This is a redirect
+      end
+
+      it "changes post count by 1" do
+        expect { post :create, post: valid_attributes }.to change(Post, :count).by(1)
+      end
+
+      it "sets a flash message" do
+        post :create, post: valid_attributes
+        expect(flash[:notice]).to_not be_blank
+      end
+    end
+
+    context "with invalid attributes" do
+      it "renders the new template" do
+        post :create, post: {title: nil}
+        expect(response).to render_template :new
+      end
+
+      it "does not create a post" do
+        expect { post :create, post: {title: nil} }.to change(Post, :count).by(0)
+      end
+    end
+  end
+
+  describe "GET 'show'" do
+    let(:post) { create(:post) }
+    it "is successful" do
+      get :show, id: post.id
+      expect(response).to be_successful
+    end
+  end
 end
