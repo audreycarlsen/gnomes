7b3ff7c70e9405cb833d3310a5a2f62c9e2f3964
Galina <yermichevagalina@gmail.com>
2014-02-12 11:39:58 -0800

    weather forecast formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,9 @@ module ApplicationHelper
   def eu
     eu = EventsUser.find_by(user_id: current_user.id, event_id:@event.id)
   end
+
+  def forecast
+    date = Date.today.beginning_of_week
+    return (date..date + 4.days).to_a
+  end
 end
