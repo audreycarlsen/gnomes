d752978b9fe49cd120fabd854a424d4239c955fa
Galina <yermichevagalina@gmail.com>
2014-02-12 22:19:28 -0800

    fixed a reserve_tool route

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Gnomes::Application.routes.draw do
   get '/auth/:provider/callback', to: 'session#create'
   post '/events/:id/:response',   to: 'events#rsvp',     as: 'rsvp'
   delete '/events/:id/no',        to: 'events#rsvp_no',  as: 'rsvp_no'
-  post '/tools/:id/reserve',      to: 'tools#reserve',   as: 'reserve_tool'
+  post '/tools/:id/reserve',      to: 'tools#reserve_tool',   as: 'reserve_tool'
   post '/tools/:id/return',       to: 'tools#return_tool',    as: 'return_tool'
 
   if Rails.env == "test"
