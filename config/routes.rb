f25cc5566f293572a5e5fe09a2ea61dd0d2ba334
Galina <yermichevagalina@gmail.com>
2014-02-16 18:00:33 -0800

    fixed rsvp route

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,8 @@ Gnomes::Application.routes.draw do
   get '/auth/twitter',                                      as: 'sign_in'
   get '/sign_out',                 to: 'session#destroy',   as: 'sign_out'
   get '/auth/:provider/callback',  to: 'session#create'
-  post '/events/:id/:response'     to: "events#rsvp",       as: 'rsvp'
-  delete '/events/:id/no'          to: "events#rsvp_no",    as: 'rsvp_no'
+  post '/events/:id/:response',    to: "events#rsvp",       as: 'rsvp'
+  delete '/events/:id/no',         to: "events#rsvp_no",    as: 'rsvp_no'
   patch '/tools/:id/reserve',      to: 'tools#reserve_tool',as: 'reserve_tool'
   patch '/tools/:id/return',       to: 'tools#return_tool', as: 'return_tool'
 
