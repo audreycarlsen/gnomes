d54d366024368c3479c130008f7d4e47ce516596
Audrey Carlsen <audreycarlsen@gmail.com>
2014-02-18 16:07:36 -0800

    i don't know

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-     @posts = Post.all.order('created_at DESC').first(3)
-     @tools = Tool.all
-     @events = Event.all
+    @posts = Post.all.order('created_at DESC').first(3)
+    @tools = Tool.all
+    @events = Event.all
+    @user = current_user
   end
 end
