27abd03d3bf6d0b46495771434b78b39a78c2a78
Galina <yermichevagalina@gmail.com>
2014-02-19 11:21:53 -0800

    calendar knows how to AJAX now toogit add .

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,86 +18,43 @@
 //               DISQUS
 //*************************************
    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-        var disqus_shortname = 'gnomesweetgnome'; // required: replace example with your forum shortname
+    var disqus_shortname = 'gnomesweetgnome'; // required: replace example with your forum shortname
 
-        /* * * DON'T EDIT BELOW THIS LINE * * */
-        (function() {
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        })();
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
 
-           (function () {
-        var s = document.createElement('script'); s.async = true;
-        s.type = 'text/javascript';
-        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
-        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-    }());
+       (function () {
+    var s = document.createElement('script'); s.async = true;
+    s.type = 'text/javascript';
+    s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+    (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
 //*************************************
 //               SEMANTIC
 //*************************************
 
-           $(document)
-  .ready(function() {
-
-    var
-      changeSides = function() {
-        $('.ui.shape')
-          .eq(0)
-            .shape('flip over')
-            .end()
-          .eq(1)
-            .shape('flip over')
-            .end()
-          .eq(2)
-            .shape('flip back')
-            .end()
-          .eq(3)
-            .shape('flip back')
-            .end()
-        ;
-      },
-      validationRules = {
-        firstName: {
-          identifier  : 'email',
-          rules: [
-            {
-              type   : 'empty',
-              prompt : 'Please enter an e-mail'
-            },
-            {
-              type   : 'email',
-              prompt : 'Please enter a valid e-mail'
-            }
-          ]
-        }
-      }
-    ;
-
-    $('.ui.dropdown')
-      .dropdown({
-        on: 'hover'
-      })
-    ;
-
-    $('.ui.form')
-      .form(validationRules, {
-        on: 'blur'
-      })
-    ;
-
-    $('.masthead .information')
-      .transition('scale in')
-    ;
-
-    setInterval(changeSides, 3000);
-
-  })
-;
-//*************************************
-//               EQUAL COLUMNS
-//*************************************
 $(document).ready(function() {
-  $(".calendar").equalHeights(100,300);
-});
+
+alert("it works");
+  $(".events_calendar").on("click", ".next-month, .previous-month", function() {
+
+  $.ajax({
+    url: $(this).attr("href"),
+    type: 'GET',
+    dataType: 'script',
+    success: function(data, textStatus, xhr){
+
+    },
+    error: function(xhr,textStatus, errorThrown) {
+
+    }
+    });
+  return false;
+  });
+
+  });
 
