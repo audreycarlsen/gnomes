8436268a002bf6c856c3f404207ceb1b06c9a171
Audrey Carlsen <audreycarlsen@gmail.com>
2014-02-20 15:55:39 -0800

    renamed user table in databse

--- db/migrate/20140210190000_create_users_table.rb
+++ db/migrate/20140210190000_create_users_table.rb
@@ -1,6 +1,6 @@
-class CreateUsersTable < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users_tables do |t|
+    create_table :users do |t|
       t.string   "username"
       t.boolean  "admin"
       t.timestamps
