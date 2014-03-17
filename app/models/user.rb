15c12422394276c9c432df32de3c3c5f44568f0d
Galina Yermicheva <yermichevagalina@gmail.com>
2014-03-17 09:19:05 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,22 @@
 class User < ActiveRecord::Base
   has_many :events, through: :events_users
-  has_many :events_users, :dependent => :destroy
+  has_many :events_users, dependent: :destroy
+  has_many :tools
+  
   validates :username, presence: true, uniqueness: true
   validates :admin, inclusion: { in: [true, false] }
   validates :uid, presence: true, uniqueness: true
-  has_many :tools
+  
 
   has_many :response_yes, through: :events_users,
-          :class_name => "Event",
-          :source => :event,
-          :conditions => ['events_users.response = ?',"yes"]
+           class_name: "Event",
+           source: :event,
+           conditions: ['events_users.response = ?',"yes"]
 
-has_many  :response_maybe, through: :events_users,
-          :class_name => "Event",
-          :source => :event,
-          :conditions => ['events_users.response = ?',"maybe"]
+ has_many  :response_maybe, through: :events_users,
+           class_name: "Event",
+           source: :event,
+           conditions: ['events_users.response = ?',"maybe"]
 
   def self.find_or_create_from_omniauth(auth_hash)
     user = User.find_by(uid: auth_hash["uid"].to_s) || User.create_from_omniauth(auth_hash)
